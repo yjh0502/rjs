0994a35220b7dce794ae55ae61100466653195d7
Magnus Ottenklinger <evnu@posteo.de>
2020-01-29 13:12:29 +0100

    Add simple unit tests

--- src/rjs.erl
+++ src/rjs.erl
@@ -4,6 +4,8 @@
 -on_load(init/0).
 -export([encode/1, decode/1, decode/2]).
 
+-include_lib("eunit/include/eunit.hrl").
+
 encode(Data) ->
     nif_encode(Data).
 
@@ -31,3 +33,59 @@ nif_encode(_Data) ->
 
 nif_decode(_Data, _Opts) ->
     ?NOT_LOADED.
+
+-ifdef(EUNIT).
+
+encode_integer_test() ->
+    ?assertEqual({ok, <<"1">>}, encode(1)),
+    ?assertEqual({ok, <<"2">>}, encode(2)),
+    ?assertEqual({ok, <<"-1">>}, encode(-1)).
+
+encode_float_test() ->
+    ?assertEqual({ok, <<"1.0">>}, encode(1.0)),
+    ?assertEqual({ok, <<"2.0">>}, encode(2.0)),
+    ?assertEqual({ok, <<"-1.0">>}, encode(-1.0)).
+
+encode_string_test() ->
+    %% Charlists are encoded as charlists
+    ?assertEqual({ok,<<"[104,101,108,108,111]">>}, encode("hello")),
+    ?assertEqual({ok, <<"\"world\"">>}, encode(<<"world">>)).
+
+encode_map_test() ->
+    ?assertEqual({ok, <<"{}">>}, encode(#{})),
+    ?assertEqual({ok, <<"{\"hello\":\"world\"}">>}, encode(#{hello => world})),
+    ?assertEqual({ok, <<"{\"1\":2}">>}, encode(#{1 => 2})).
+
+encode_null_test() ->
+    ?assertEqual({ok, <<"null">>}, encode(null)).
+
+encode_list_test() ->
+    ?assertEqual({ok, <<"[]">>}, encode([])),
+    ?assertEqual({ok, <<"[1]">>}, encode([1])),
+    ?assertEqual({ok, <<"[1,-1.0]">>}, encode([1, -1.0])).
+
+decode_integer_test() ->
+    ?assertEqual({ok, 1}, decode(<<"1">>)),
+    ?assertEqual({ok, 2}, decode(<<"2">>)),
+    ?assertEqual({ok, -1}, decode(<<"-1">>)).
+
+decode_float_test() ->
+    ?assertEqual({ok, 1.0}, decode(<<"1.0">>)),
+    ?assertEqual({ok, 2.0}, decode(<<"2.0">>)),
+    ?assertEqual({ok, -1.0}, decode(<<"-1.0">>)).
+
+decode_string_test() ->
+    ?assertEqual({ok, "hello"}, decode(<<"[104,101,108,108,111]">>)),
+    ?assertEqual({ok, <<"world">>}, decode(<<"\"world\"">>)).
+
+decode_null_test() ->
+    ?assertEqual({ok, null}, decode(<<"null">>)).
+
+decode_map_test() ->
+    ?assertEqual({ok, #{}}, decode(<<"{}">>)),
+    ?assertEqual({ok, #{<<"hello">> => <<"world">>}}, decode(<<"{\"hello\":\"world\"}">>)),
+    ?assertEqual({ok, #{<<"1">> => 2}}, decode(<<"{\"1\":2}">>)),
+    ?assertEqual({ok, #{hello => <<"world">>}}, decode(<<"{\"hello\":\"world\"}">>, [atom])),
+    ?assertEqual({ok, #{hello => <<"world">>}}, decode(<<"{\"hello\":\"world\"}">>, [existing_atom])).
+
+-endif.
